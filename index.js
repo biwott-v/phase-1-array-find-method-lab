48f99a5dd803994fc40a693dbc2d520202d85806
biwott-v <victor.biwott@student.moringaschool.com>
2025-03-14 13:32:58 +0300

    completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    let value = record.find((element) => element.result==="W");
+    if (value){
+        return value.year;
+    }    
+  return value;
+}
+
